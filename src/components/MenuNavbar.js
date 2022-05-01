5edfbfaa7252d29c7d059349bef9d9c276a3df90
Fridabolin <fridasofia.bolin@gmail.com>
2022-05-01 19:46:38 +0200

    navbar kopplad till usercontex och styling

--- src/components/MenuNavbar.js
+++ src/components/MenuNavbar.js
@@ -1,6 +1,7 @@
 //react
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { UserContext } from "../context/UserContext";
+import { useNavigate } from "react-router-dom";
 
 
 
@@ -13,173 +14,201 @@ import { Container } from 'react-bootstrap'
 import { FormControl } from 'react-bootstrap'
 import { Form } from 'react-bootstrap'
 import {LinkContainer} from 'react-router-bootstrap'
-
+import Button from 'react-bootstrap/Button'
 
 //Styling
 import "./MenuNavbar.scss"
+import navImage from "../assets/navImage.jpg"
 
 const MenuNavbar = () => {
-
+  
   const { loggedIn, setLoggedIn, setUser } = useContext(UserContext); 
 
+  const navigate = useNavigate()
   
+  const handleLogOut = () => {
+    setLoggedIn(false);
+    setUser({});
+    navigate("main");
+  };
+
+
+  //för dropdown
+  const [show, setShow] = useState(false);
+  const showDropdown = (e)=>{
+      setShow(!show);
+  }
+  const hideDropdown = e => {
+      setShow(false);
+  }
+
+  //Här är du inloggad
   const LoggedIn = () => {
     return(
       <>
-      
-  <Navbar bg="black" expand="md">
-    <Container fluid>
-      <Navbar.Brand href="#">Cool Fashion</Navbar.Brand>
-      <Navbar.Toggle aria-controls="navbarScroll" />
-      <Navbar.Collapse id="navbarScroll">
-        <Nav
-          className="me-auto my-2 my-lg-0"
-          navbarScroll
-        >
-  
-          <NavDropdown  title="Shop" id="navbarScrollingDropdown">
-            <div className="categories">
-            <NavDropdown.Divider /> 
-            <p>Kategorier</p>
-            <NavDropdown.Divider />
-            <NavDropdown.Item href="#action3">Byxor</NavDropdown.Item>
-            <NavDropdown.Item href="#action4">Jumpsuits</NavDropdown.Item>
-            <NavDropdown.Item href="#action4">Toppar & T-shirts</NavDropdown.Item>
-            <NavDropdown.Item href="#action4">Tröjor & Sweatshirts</NavDropdown.Item>
-            <NavDropdown.Item href="#action4">Klänningar</NavDropdown.Item>
-            <NavDropdown.Item href="#action4">Kjolar</NavDropdown.Item>
-            <NavDropdown.Item href="#action4">Matchande set</NavDropdown.Item>
-            <NavDropdown.Item href="#action4">Underkläder</NavDropdown.Item>
-            <NavDropdown.Item href="#action4">Ytterkläder</NavDropdown.Item>
-            </div>
-  
-            <div className="inspiration">
-            <NavDropdown.Divider /> 
-            <p> Inspiration och Nyheter</p>
-            <NavDropdown.Divider />
-            <NavDropdown.Item href="#action4">Vårnyheter</NavDropdown.Item>
-            <NavDropdown.Item href="#action4">Student</NavDropdown.Item>
-            <NavDropdown.Item href="#action4">Festival</NavDropdown.Item>
-            <NavDropdown.Item href="#action4">Tropical vaycay</NavDropdown.Item>
-            <NavDropdown.Item href="#action4">Summer in the city</NavDropdown.Item>
-            <NavDropdown.Item href="#action4">Boho summer</NavDropdown.Item>
-            <NavDropdown.Item href="#action4">TikTok vibe</NavDropdown.Item>
-            <NavDropdown.Item href="#action4">Stand out from the crowd</NavDropdown.Item>
-            </div>
-            <div className="dropdown_img">
-            <NavDropdown.Divider /> 
-            <p>Senaste</p>
-            <NavDropdown.Divider />
-              Här ska en bild vara
+        <Navbar bg="black" expand="lg">
+          <Container fluid>
+            <Navbar.Brand href="#">Cool Fashion</Navbar.Brand>
+            <Navbar.Toggle aria-controls="navbarScroll" />
+            <Navbar.Collapse id="navbarScroll">
+              <Nav
+                className="me-auto my-2 my-lg-0"
+                navbarScroll
+              >
+        
+                <NavDropdown  
+                title="SHOP"
+                id="navbarScrollingDropdown"
+                show={show}  
+                onMouseEnter={showDropdown}
+                onMouseLeave={hideDropdown}
+                >
+                  <div className="categories">
+                  <NavDropdown.Divider /> 
+                  <p>Kategorier</p>
+                  <NavDropdown.Divider />
+                  <NavDropdown.Item href="#action3">Byxor</NavDropdown.Item>
+                  <NavDropdown.Item href="#action4">Jumpsuits</NavDropdown.Item>
+                  <NavDropdown.Item href="#action4">Toppar & T-shirts</NavDropdown.Item>
+                  <NavDropdown.Item href="#action4">Tröjor & Sweatshirts</NavDropdown.Item>
+                  <NavDropdown.Item href="#action4">Klänningar</NavDropdown.Item>
+                  <NavDropdown.Item href="#action4">Kjolar</NavDropdown.Item>
+                  <NavDropdown.Item href="#action4">Matchande set</NavDropdown.Item>
+                  <NavDropdown.Item href="#action4">Underkläder</NavDropdown.Item>
+                  <NavDropdown.Item href="#action4">Ytterkläder</NavDropdown.Item>
+                  </div>
+        
+                  <div className="inspiration">
+                  <NavDropdown.Divider /> 
+                  <p> Inspiration och Nyheter</p>
+                  <NavDropdown.Divider />
+                  <NavDropdown.Item href="#action4">Vårnyheter</NavDropdown.Item>
+                  <NavDropdown.Item href="#action4">Student</NavDropdown.Item>
+                  <NavDropdown.Item href="#action4">Festival</NavDropdown.Item>
+                  <NavDropdown.Item href="#action4">Tropical vaycay</NavDropdown.Item>
+                  <NavDropdown.Item href="#action4">Summer in the city</NavDropdown.Item>
+                  <NavDropdown.Item href="#action4">Boho summer</NavDropdown.Item>
+                  <NavDropdown.Item href="#action4">TikTok vibe</NavDropdown.Item>
+                  <NavDropdown.Item href="#action4">Stand out from the crowd</NavDropdown.Item>
+                  </div>
+                  <div className="dropdown_img">
+                  <NavDropdown.Divider /> 
+                  <p>Senaste</p>
+                  <NavDropdown.Divider />
+                  <img src={navImage} style={{width:"10rem"}}/>
+                    </div>
+              </NavDropdown>
+        
+        
+              </Nav>
+
+              <div className="links_end">
+              <Form className="d-flex">
+                <FormControl
+                  type="search"
+                  placeholder="Search"
+                  className="me-2"
+                  aria-label="Search"
+                />
+              </Form>
+              <NavDropdown 
+              className="my_account" 
+              title="Mitt konto"
+              id="navbarScrollingDropdown"
+              >
+                  <NavDropdown.Item href="#action3">Mina köp</NavDropdown.Item>
+                  <NavDropdown.Item href="#action4">Retunera</NavDropdown.Item>
+                  <NavDropdown.Divider />
+                  <NavDropdown.Item href="#action5">
+                  <Button  as="input" type="submit" value="LOGGA UT" onClick={handleLogOut} />
+                  </NavDropdown.Item>
+                </NavDropdown>
+                <LinkContainer to="/checkout">
+                  <Nav.Link>checkout</Nav.Link>
+                  </LinkContainer>
               </div>
-         </NavDropdown>
-  
-  
-        </Nav>
-        <Form className="d-flex">
-          <FormControl
-            type="search"
-            placeholder="Search"
-            className="me-2"
-            aria-label="Search"
-          />
-        </Form>
-        <NavDropdown className="my_account" title="Mitt konto" id="navbarScrollingDropdown">
-            <NavDropdown.Item href="#action3">Mina köp</NavDropdown.Item>
-            <NavDropdown.Item href="#action4">Retunera</NavDropdown.Item>
-            <NavDropdown.Divider />
-            <NavDropdown.Item href="#action5">
-              Logga ut
-            </NavDropdown.Item>
-          </NavDropdown>
-  
-          <LinkContainer to="/checkout">
-            <Nav.Link>Kassa</Nav.Link>
-            </LinkContainer>
-      </Navbar.Collapse>
-    </Container>
-  </Navbar>
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
       </>
    )
   }
   
+  //här är du utloggad
   const LoggedOut = () =>{
     return(
       <>
-      
-  <Navbar bg="black" expand="md">
-    <Container fluid>
-      <Navbar.Brand href="#">Cool Fashion</Navbar.Brand>
-      <Navbar.Toggle aria-controls="navbarScroll" />
-      <Navbar.Collapse id="navbarScroll">
-        <Nav
-          className="me-auto my-2 my-lg-0"
-          navbarScroll
-        >
-  
-          <NavDropdown  title="Shop" id="navbarScrollingDropdown">
-            <div className="categories">
-            <NavDropdown.Divider /> 
-            <p>Kategorier</p>
-            <NavDropdown.Divider />
-            <NavDropdown.Item href="#action3">Byxor</NavDropdown.Item>
-            <NavDropdown.Item href="#action4">Jumpsuits</NavDropdown.Item>
-            <NavDropdown.Item href="#action4">Toppar & T-shirts</NavDropdown.Item>
-            <NavDropdown.Item href="#action4">Tröjor & Sweatshirts</NavDropdown.Item>
-            <NavDropdown.Item href="#action4">Klänningar</NavDropdown.Item>
-            <NavDropdown.Item href="#action4">Kjolar</NavDropdown.Item>
-            <NavDropdown.Item href="#action4">Matchande set</NavDropdown.Item>
-            <NavDropdown.Item href="#action4">Underkläder</NavDropdown.Item>
-            <NavDropdown.Item href="#action4">Ytterkläder</NavDropdown.Item>
-            </div>
-  
-            <div className="inspiration">
-            <NavDropdown.Divider /> 
-            <p> Inspiration och Nyheter</p>
-            <NavDropdown.Divider />
-            <NavDropdown.Item href="#action4">Vårnyheter</NavDropdown.Item>
-            <NavDropdown.Item href="#action4">Student</NavDropdown.Item>
-            <NavDropdown.Item href="#action4">Festival</NavDropdown.Item>
-            <NavDropdown.Item href="#action4">Tropical vaycay</NavDropdown.Item>
-            <NavDropdown.Item href="#action4">Summer in the city</NavDropdown.Item>
-            <NavDropdown.Item href="#action4">Boho summer</NavDropdown.Item>
-            <NavDropdown.Item href="#action4">TikTok vibe</NavDropdown.Item>
-            <NavDropdown.Item href="#action4">Stand out from the crowd</NavDropdown.Item>
-            </div>
-            <div className="dropdown_img">
-            <NavDropdown.Divider /> 
-            <p>Senaste</p>
-            <NavDropdown.Divider />
-              Här ska en bild vara
-              </div>
-         </NavDropdown>
-  
-  
-        </Nav>
-        <Form className="d-flex">
-          <FormControl
-            type="search"
-            placeholder="Search"
-            className="me-2"
-            aria-label="Search"
-          />
-        </Form>
-        <LinkContainer to="/checkout">
-            <Nav.Link>Logga in / Skapa konto</Nav.Link>
-            </LinkContainer>
-        <LinkContainer to="/checkout">
-          <Nav.Link>Kassa</Nav.Link>
-          </LinkContainer>
-      </Navbar.Collapse>
-    </Container>
-  </Navbar>
+          <Navbar bg="black" expand="lg">
+            <Container fluid>
+              <Navbar.Brand href="#">Cool Fashion</Navbar.Brand>
+              <Navbar.Toggle className="hamburger" aria-controls="navbarScroll" />
+              <Navbar.Collapse id="navbarScroll">
+                <Nav className="me-auto my-2 my-lg-0" navbarScroll>
+                  <NavDropdown  
+                  show={show}  
+                  onMouseEnter={showDropdown}
+                  onMouseLeave={hideDropdown}
+                  title="SHOP" 
+                  id="navbarScrollingDropdown">
+                    <div className="categories">
+                    <NavDropdown.Divider /> 
+                    <p>Kategorier</p>
+                    <NavDropdown.Divider />
+                    <NavDropdown.Item href="#action3">Byxor</NavDropdown.Item>
+                    <NavDropdown.Item href="#action4">Jumpsuits</NavDropdown.Item>
+                    <NavDropdown.Item href="#action4">Toppar & T-shirts</NavDropdown.Item>
+                    <NavDropdown.Item href="#action4">Tröjor & Sweatshirts</NavDropdown.Item>
+                    <NavDropdown.Item href="#action4">Klänningar</NavDropdown.Item>
+                    <NavDropdown.Item href="#action4">Kjolar</NavDropdown.Item>
+                    <NavDropdown.Item href="#action4">Matchande set</NavDropdown.Item>
+                    <NavDropdown.Item href="#action4">Underkläder</NavDropdown.Item>
+                    <NavDropdown.Item href="#action4">Ytterkläder</NavDropdown.Item>
+                    </div>
+          
+                    <div className="inspiration">
+                    <NavDropdown.Divider /> 
+                    <p> Inspiration och Nyheter</p>
+                    <NavDropdown.Divider />
+                    <NavDropdown.Item href="#action4">Vårnyheter</NavDropdown.Item>
+                    <NavDropdown.Item href="#action4">Student</NavDropdown.Item>
+                    <NavDropdown.Item href="#action4">Festival</NavDropdown.Item>
+                    <NavDropdown.Item href="#action4">Tropical vaycay</NavDropdown.Item>
+                    <NavDropdown.Item href="#action4">Summer in the city</NavDropdown.Item>
+                    <NavDropdown.Item href="#action4">Boho summer</NavDropdown.Item>
+                    <NavDropdown.Item href="#action4">TikTok vibe</NavDropdown.Item>
+                    <NavDropdown.Item href="#action4">Stand out from the crowd</NavDropdown.Item>
+                    </div>
+                    <div className="dropdown_img">
+                    <NavDropdown.Divider /> 
+                    <p>Senaste</p>
+                    <NavDropdown.Divider />
+                      <img src={navImage} style={{width:"10rem"}}/>
+                      </div>
+                </NavDropdown>
+          
+          
+                </Nav>
+                <Form className="d-flex">
+                  <FormControl
+                    type="search"
+                    placeholder="Search"
+                    className="me-2"
+                    aria-label="Search"
+                  />
+                </Form>
+                <Button  as="input" type="submit" value="LOGGA IN" onClick={() => navigate("/login")} />
+                <LinkContainer to="/checkout">
+                  <Nav.Link>Kassa</Nav.Link>
+                  </LinkContainer>
+              </Navbar.Collapse>
+            </Container>
+          </Navbar>
       </>
    )
   }
+   
+  //kollar om du är in eller utloggad
   return <>{loggedIn ? LoggedIn() : LoggedOut()}</>;
-
- 
-
 
 };
 
--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import './index.scss';
 import App from './App';
 import UserProvider from "./context/UserContext";
 
+
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
