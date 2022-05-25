f58d4e8079d385157d438eeb22a4cf72700435b5
Fridabolin <fridasofia.bolin@gmail.com>
2022-05-25 11:14:18 +0200

    går att byta rubrik(ej bild)

--- src/pages/admin/UpdateFrontAdmin.js
+++ src/pages/admin/UpdateFrontAdmin.js
@@ -1,13 +1,23 @@
 import NavbarAdmin from './components/NavbarAdmin'
-import React from 'react';
+import React, {useState, useContext } from 'react';
 import Uploadingimg from "./components/Uploadimg"
+import { ImageContext } from '../../context/ImageContext';
 
 //Style
 import "./scss/updatefrontadmin.scss"
 
 const UpdateFrontAdmin = () => {
-
-
+  
+  const { headline, setHeadline } = useContext(ImageContext);
+  const [message, setMessage] = useState("")
+  
+  const handleInput = (e) => {
+    setHeadline({ ...headline, [e.target.name]: e.target.value });
+  }
+  const handleClick = () => {
+    alert("är du säker på att du vill ändra dina bilder")
+    setMessage("Dina bilder är nu uppdaterade")
+  }
 
   return (
     <div style={{height:"100vh", display:"flex"}}>
@@ -40,36 +50,63 @@ const UpdateFrontAdmin = () => {
       </div>
     
     <div className='rowTwo'>
-      <h2>Valda underrubriker</h2>
+      <h2>Valda underrubriker till bildspel</h2>
         <div className='wrapperRubrik'>
-          <div>
+          <div className='rubrikDiv'>
             <p>Rubrik 1</p>
-            <input></input>
+            <input 
+                name="headline1"
+                placeholder="rubrik till bild 1"
+                onChange={handleInput}
+                required
+            />
           </div>
-          <div>
+          <div  className='rubrikDiv'>
             <p>Rubrik 2</p>
-            <input></input>
+            <input
+               name="headline2"
+               placeholder="rubrik till bild 2"
+               onChange={handleInput}
+               required
+            />
           </div>
-          <div>
+          <div  className='rubrikDiv'>
             <p>Rubrik 3</p>
-            <input></input>
+            <input     
+               name="headline3"
+               placeholder="rubrik till bild 3"
+               onChange={handleInput}
+               required
+            />
           </div>
-          <div>
+          <div  className='rubrikDiv'>
             <p>Rubrik 1</p>
-            <input></input>
+            <input     
+               name="headline4"
+               placeholder="rubrik till bild 4"
+               onChange={handleInput}
+               required
+            />
           </div>
-        <div>
+        <div  className='rubrikDiv'>
           <p>Rubrik 4</p>
-          <input></input>
+          <input     name="headline5"
+               placeholder="rubrik till bild 5"
+               onChange={handleInput}
+               required
+            />
         </div>
-        <div>
-          <p>Rubrik 5</p>
-          <input></input>
-        </div>
+    
         </div>
       </div>
-      
-      <button>SPARA ÄNDRINGAR</button>
+
+      <div className='rowThree'>
+      <div className='confirmMessage'><p>{message}</p></div>
+      <button 
+      className='buttonImg'
+      onClick={handleClick}>SPARA ÄNDRINGAR</button>
+      </div>
+
       </div>
 
     </div>
--- src/pages/main/shopByCategory.js
+++ src/pages/main/shopByCategory.js
@@ -5,9 +5,27 @@ import klänning_kjolar from '../../assets/images/KATEGORIER/klänning_kjol.jpg'
 import skjortor from '../../assets/images/KATEGORIER/skjortor.jpg'
 import ytterkläder from '../../assets/images/KATEGORIER/ytterkläder.jpg'
 import { useNavigate } from 'react-router-dom'
+//för att ändra rubrikerna
+import { useContext, useState } from 'react'
+import { ImageContext } from '../../context/ImageContext'
+
 
 const ShopByCategory = () => {
   const navigate = useNavigate()
+
+  const { headline } = useContext(ImageContext);
+  
+  const [originalHeadline] = useState({
+    headlineOrg1: headline.headline1 || "BYXOR",
+    headlineOrg2: headline.headline2 ||  "TOPPAR",
+    headlineOrg3: headline.headline3 || "KLÄNNINGAR & KJOLAR",
+    headlineOrg4: headline.headline4 || "SKJORTOR",
+    headlineOrg5: headline.headline5 || "YTTERKLÄDER",
+   
+  })
+
+
+
 
   return (
     <>
@@ -25,7 +43,7 @@ const ShopByCategory = () => {
                 <p>VISA BYXOR</p>
               </figcaption>
             </div>
-            <b>BYXOR</b>
+            <b>{originalHeadline.headlineOrg1}</b>
           </div>
 
           <div onClick={() => navigate('/tops')} className="category-wrapper">
@@ -35,7 +53,7 @@ const ShopByCategory = () => {
                 <p>VISA TOPPAR</p>
               </figcaption>
             </div>
-            <b>TOPPAR</b>
+            <b>{originalHeadline.headlineOrg2}</b>
           </div>
 
           <div
@@ -48,7 +66,7 @@ const ShopByCategory = () => {
                 <p> VISA KLÄNNINGAR & KJOLAR</p>
               </figcaption>
             </div>
-            <b>KLÄNNINGAR & KJOLAR</b>
+            <b> <b>{originalHeadline.headlineOrg3}</b></b>
           </div>
 
           <div onClick={() => navigate('/shirts')} className="category-wrapper">
@@ -58,7 +76,7 @@ const ShopByCategory = () => {
                 <p>VISA SKJORTOR</p>
               </figcaption>
             </div>
-            <b>SKJORTOR</b>
+            <b> <b>{originalHeadline.headlineOrg4}</b></b>
           </div>
 
           <div
@@ -71,7 +89,7 @@ const ShopByCategory = () => {
                 <p>VISA YTTERKLÄDER</p>
               </figcaption>
             </div>
-            <b>YTTERKLÄDER</b>
+            <b> <b>{originalHeadline.headlineOrg5}</b></b>
           </div>
         </div>
       </div>
