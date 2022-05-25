45878f6ea488e3226dd20230c3b631ab258da977
Amalia <74189129+chocolateprincess666@users.noreply.github.com>
2022-05-25 16:48:51 +0200

    funktioner för varukorg och checkout sidan

--- src/pages/productpages/displayProducts.js
+++ src/pages/productpages/displayProducts.js
@@ -1,9 +1,25 @@
 import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
+import { useContext } from 'react';
+import { CartContext } from '../../context/CartContext';
 
 const DisplayProds = (props) => {
 
-    const {product} = props;
+    const { product } = props;
+    
+    const { cart, setCart } = useContext(CartContext);
+
+     //to not dublicate item, but at the same time add item
+     const addProducts = (product) => {
+        const exist = cart.find(x => x.id === product.id);
+        if(exist) {
+            setCart(cart.map((x => x.id === product.id ? {...exist, 
+            quantity: exist.quantity + 1} 
+            : x)))
+        } else {
+            setCart([...cart, {...product, quantity: 1}]);
+        }
+    }
 
     return (
         <>
@@ -14,7 +30,7 @@ const DisplayProds = (props) => {
                             <Card.Text>
                                 {product.price}:-
                             </Card.Text>
-                        <Button variant="primary" onClick="">Köp</Button>
+                        <Button variant="primary" onClick={() => addProducts(product)}>Köp</Button>
                     </Card.Body>
             </Card>
         </>
